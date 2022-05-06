cca33f42b5ab4cdfc3644a4f0fc96b6442877132
Bruno Veloso <brvelosoo@gmail.com>
2022-05-05 21:32:05 -0300

    Componente loading

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,11 +3,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand:{
+        brand:{ 
+          300: '#996DFF',
           500: '#8257e6',
         }
+      },
+      borderRadius:{
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
