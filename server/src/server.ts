ee5016f18eb5607df427a8cfbe54b8326065bd66
Bruno Veloso <brvelosoo@gmail.com>
2022-05-23 16:18:45 -0300

    Configuração de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req,res) =>{
-    return res.send('Hello Word!');
+app.use(express.json())
+
+app.post('/feedbacks', async (req,res) =>{
+    const {type, comment, screeshot} = req.body;
+    const feedback = await prisma.feedback.create({
+        data:{
+            type,
+            comment,
+            screeshot,
+        }
+    })
+    return res.status(201).json({date : feedback})
 })
 
 app.listen(3333, () => {
