29a7d2c49b7f08f432f0e9bc46e31c12d4d378c4
Bruno Veloso <brvelosoo@gmail.com>
2022-06-02 14:10:29 -0300

    Botao flutuante

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,41 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { theme } from './src/theme';
+import { Widget } from './src/components/Widget';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+
+      <Widget />
+
+      <StatusBar
+        style="light"
+        backgroundColor='transparent'
+        translucent
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
