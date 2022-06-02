576ba821265fa13bd4e5a13ba183aa35a8109e83
Bruno Veloso <brvelosoo@gmail.com>
2022-06-02 16:10:41 -0300

    Opcoes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget';
+import  Widget  from './src/components/Widget';
 import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
@@ -26,14 +26,12 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-
-      <Widget />
-
       <StatusBar
         style="light"
         backgroundColor='transparent'
         translucent
       />
+      <Widget />
     </View>
   );
 }
